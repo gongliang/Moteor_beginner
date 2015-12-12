529f92a5de950759146fc7607753101aa0619be7
gongliang <272455414@qq.com>
2015-12-12 21:24:22 +0800

    修改自动发布为手写自动发布函数

--- myapp/client/templates/posts/post_list.js
+++ myapp/client/templates/posts/post_list.js
@@ -1,29 +1,32 @@
-var postsData = [
-    {
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    },
-    {
-        title: '百度',
-        url: 'http:/baidu.com/'
-    },
-    {
-        title: '芒果tv',
-        url: 'http://mgtv.com'
-    },
-    {
-        title: '凤凰网',
-        url: 'http://ifeng.com'
-    }
-];
+// var postsData = [
+//     {
+//         title: 'Introducing Telescope',
+//         url: 'http://sachagreif.com/introducing-telescope/'
+//     },
+//     {
+//         title: 'Meteor',
+//         url: 'http://meteor.com'
+//     },
+//     {
+//         title: 'The Meteor Book',
+//         url: 'http://themeteorbook.com'
+//     },
+//     {
+//         title: '百度',
+//         url: 'http:/baidu.com/'
+//     },
+//     {
+//         title: '芒果tv',
+//         url: 'http://mgtv.com'
+//     },
+//     {
+//         title: '凤凰网',
+//         url: 'http://ifeng.com'
+//     }
+// ];
 Template.postsList.helpers({
-    posts: postsData
+    // posts: postsData
+    posts: function () {
+        return Posts.find();
+    }
 });
