e39ce33f444d6725aebc26bacbda3503b0adfd69
龚亮 <272455414@qq.com>
2015-12-12 19:40:31 +0800

    添加新文件到git仓库

---
+++ myapp/client/templates/posts/post_list.js
@@ -0,0 +1,29 @@
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    },
+    {
+        title: '百度',
+        url: 'http:/baidu.com/'
+    },
+    {
+        title: '芒果tv',
+        url: 'http://mgtv.com'
+    },
+    {
+        title: '凤凰网',
+        url: 'http://ifeng.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
